336d4d5abfc051f2570c919d65f3adf3c9486173
Cristoffer Dessezar <dessezar@outlook.com>
2026-01-06 20:51:03 +0100

    added side menu

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,8 +9,32 @@
     @livewireStyles
 </head>
 <body class="min-h-screen bg-gray-100">
-    {{ $slot }}
+    <div class="flex min-h-screen">
+        <aside class="w-64 bg-white border-r">
+            <div class="p-4 font-bold text-lg">
+                Proxicore CRM
+            </div>
 
+            <nav class="px-2 space-y-1">
+                <a
+                    href="/customers"
+                    class="block px-3 py-2 rounded hover:bg-gray-100"
+                >
+                    Kundlista
+                </a>
+
+                <a
+                    href="/customers/create"
+                    class="block px-3 py-2 rounded hover:bg-gray-100"
+                >
+                    Kunddetaljer
+                </a>
+            </nav>
+        </aside>
+        <main class="flex-1 p-6">
+            {{ $slot }}
+        </main>
+    </div>
     @livewireScripts
 </body>
 </html>
