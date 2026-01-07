b1be9159fae07e79be61990e16d8276568205fbc
Cristoffer Dessezar <dessezar@outlook.com>
2026-01-07 18:40:58 +0100

    created model, view and routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 use App\Livewire\Customers\CustomerList;
+use App\Livewire\Customers\CustomerForm;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,5 @@ Route::get('/', function () {
 
 Route::get('/counter', Counter::class);
 Route::get('/customers', CustomerList::class)->name('customers.index');
+Route::get('/customers/create', CustomerForm::class)->name('customers.create');
+Route::get('/customers/{customer}', CustomerForm::class)->name('customers.edit');
